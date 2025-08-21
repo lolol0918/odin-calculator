0098d59e3bdec1e7c8599988afd3651ec5a595bc
Derek <derekdvalerio@gmail.com>
2025-08-21 15:24:40 +0800

    variable renaming

--- static/script/script.js
+++ static/script/script.js
@@ -1,1 +1,32 @@
-console.log("Hello, world.");
+const numberButtons = document.querySelectorAll(".number");
+const display = document.getElementById("display");
+
+const clear = () => {
+    display.value = 0;
+}
+
+const clearBtn = document.getElementById("clear");
+
+clearBtn.addEventListener("click", clear());
+
+
+numberButtons.forEach(numberButton => {
+    numberButton.addEventListener("click", () => {
+        const value = numberButton.dataset.value;
+
+        // IF input is NaN
+            // IF it is an operator
+                // store
+            // ELSE IF it is delete
+                // Delete the end of the value
+            // ELSE IF clear
+                // clear the value
+
+        if (display.value == 0) {
+            display.value = value;
+        } else {
+            display.value += value;
+        }
+        
+    });
+});
